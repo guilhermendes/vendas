60813f02d14c0090de5a865a26c3af009d6e89c6
guilherme.mendes <guilherme.mendes@seduc.ce.gov.br>
2022-08-04 13:22:08 -0300

    aula 17

---
+++ src/main/java/io/github/guilhermendes/Animal.java
@@ -0,0 +1,5 @@
+package io.github.guilhermendes;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/io/github/guilhermendes/VendasAplication.java
+++ src/main/java/io/github/guilhermendes/VendasAplication.java
@@ -1,7 +1,11 @@
 package io.github.guilhermendes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,16 @@ public class VendasAplication {
 
     @Value("${application.name}")
     private String applicationName;
+
+    @Gato
+    private Animal animal;
+
+    @Bean(name = "executarAnimal")
+    public CommandLineRunner executar(){
+        return args -> {
+          this.animal.fazerBarulho();
+        };
+    }
 
     @GetMapping("/hello")
     public String helloWorld(){
